f10ad25c3a5288cbcbeb131b46240f0513e6ae58
Charlotte Saethre <charlotte.saethre@gmail.com>
2021-01-07 10:13:53 -0700

    Modifies first view

--- MealPlanner/planner/views.py
+++ MealPlanner/planner/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("This is where you can generate and plan meals for the week!")

ade20723434aa1133657d6dc3d5546090e6e650e
Charlotte Saethre <charlotte.saethre@gmail.com>
2021-01-07 11:39:45 -0700

    Adds Meals model

--- MealPlanner/planner/models.py
+++ MealPlanner/planner/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Meals(models.Model):
+	MEAL_TYPES = [('B', "Breakfast"), ('L', "Lunch"), ('D', "Dinner")]
+	meal_title = models.CharField(max_length = 100)
+	meal_type = models.CharField(max_length = 10, choices = MEAL_TYPES, blank=True)

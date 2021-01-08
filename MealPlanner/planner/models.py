477758420eea7c0f92f5286f47359283b508b058
Charlotte Saethre <charlotte.saethre@gmail.com>
2021-01-07 17:34:56 -0700

    Adds __str__ method to Meals model

--- MealPlanner/planner/models.py
+++ MealPlanner/planner/models.py
@@ -4,3 +4,5 @@ class Meals(models.Model):
 	MEAL_TYPES = [('B', "Breakfast"), ('L', "Lunch"), ('D', "Dinner")]
 	meal_title = models.CharField(max_length = 100)
 	meal_type = models.CharField(max_length = 10, choices = MEAL_TYPES, blank=True)
+	def __str__(self):
+		return self.meal_title

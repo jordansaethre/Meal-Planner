08cf97f8934df0305d29097f4d19a1b7c4901c56
Charlotte Saethre <charlotte.saethre@gmail.com>
2021-02-09 09:38:06 -0700

    updated meals.csv to reflect models and updated models.py to include snack type

--- MealPlanner/planner/models.py
+++ MealPlanner/planner/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Meals(models.Model):
-	MEAL_TYPES = [('B', "Breakfast"), ('L', "Lunch"), ('D', "Dinner")]
+	MEAL_TYPES = [('B', "Breakfast"), ('L', "Lunch"), ('D', "Dinner"), ('S', "Snack")]
 	meal_title = models.CharField(max_length = 100)
 	meal_type = models.CharField(max_length = 10, choices = MEAL_TYPES, blank=True)
 	def __str__(self):
